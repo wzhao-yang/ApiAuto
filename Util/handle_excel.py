a03f1f3c20bf3a68b4f2e6dacef1d1f9b8f5f4b7
wzhao-yang <wzhao_yang@sina.cn>
2020-05-27 02:07:28 +0800

    对操作excel方法进行封装

--- Util/handle_excel.py
+++ Util/handle_excel.py
@@ -6,22 +6,43 @@ import openpyxl
 
 base_path = os.getcwd()
 sys.path.append(base_path)
-#  整个对象 open_excel
-open_excel = openpyxl.load_workbook("../Case/ApiAuto.xlsx")  # 首先对这个对象进行获取load本工程下得excel数据
-# open_excel = openpyxl.load_workbook(base_path+"/Case/ApiAuto.xlsx")  # 首先对这个对象进行获取load本工程下得excel数据
-sheet_name = open_excel.sheetnames  # 获取整个excel下得sheet页得内容
-excel_value = open_excel[sheet_name[0]]  # 获取第[0]个sheet页下所有数据
-print(excel_value)
-print(base_path)
-print(excel_value.cell(1, 3))
-print(excel_value.cell(1, 3).value)  # 利用cell获取坐标下数据，第1行，第3列
-print(excel_value.max_row)  # 利用max_row获取excel中有数据得行数,
 
-'''
-底层，获取数据得阀值
-min_row = MinMax(min=1, max=1000000, expected_type=int)
-max_row = MinMax(min=1, max=1000000, expected_type=int)
-min_col = MinMax(min=1, max=16384, expected_type=int)
-max_col = MinMax(min=1, max=16384, expected_type=int)
-'''
 
+class HandExcel:
+
+    # 加载excel
+    def load_excel(self):
+        open_excel = openpyxl.load_workbook("../Case/ApiAuto.xlsx")  # 首先对这个对象进行获取load本工程下得excel数据
+        return open_excel
+
+    # 加载所有sheet得内容
+    def get_sheet_data(self, index=None):
+        sheet_name = self.load_excel().sheetnames
+        if index is None:
+            index = 0
+        data = self.load_excel()[sheet_name[index]]
+        return data
+
+    # 获取一个单元格得内容是由’行‘和‘列’决定
+    def get_cell_value(self, row, cols):
+        # get_sheet_data()使用默认值0
+        data = self.get_sheet_data().cell(row=row, column=cols).value
+        return data
+
+    # 获取sheet页中得case行数
+    def get_rows(self):
+        row = self.get_sheet_data().max_row
+        return row
+
+    # 某一行得内容得list
+    def get_rows_value(self, row):
+        row_list = []
+        for i in self.get_sheet_data()[row]:
+            row_list.append(i.value)
+        return row_list
+
+
+if __name__ == "__main__":
+    handle = HandExcel()
+    print(handle.get_cell_value(2, 5))
+    print(handle.get_rows_value(2))  # 打印第2行内所有内容
